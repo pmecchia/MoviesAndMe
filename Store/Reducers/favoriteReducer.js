7f3f2e62af19b40861516d4eb8a4bf6479f7dccf
pmecchia <mecchia.pierre@gmail.com>
2020-07-10 20:32:18 +0200

    add comment

--- Store/Reducers/favoriteReducer.js
+++ Store/Reducers/favoriteReducer.js
@@ -5,13 +5,13 @@ function toggleFavorite(state = initialState,action){
   switch (action.type) {
     case 'TOGGLE_FAVORITE':
       const favoriteFilmIndex = state.favoritesFilm.findIndex(item => item.id === action.value.id)
-      if(favoriteFilmIndex !== -1){
+      if(favoriteFilmIndex !== -1){ // Le film est déjà dans les favoris, on le supprime de la liste
         nextState = {
           ...state,
           favoritesFilm: state.favoritesFilm.filter((item, index) => index !== favoriteFilmIndex)
         }
       }
-      else {
+      else { // Le film n'est pas dans les films favoris, on l'ajoute à la liste
         nextState = {
           ...state,
           favoritesFilm: [...state.favoritesFilm, action.value]
