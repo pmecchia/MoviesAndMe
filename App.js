33ec295c5c64b18a12db36b34506567d7c5f912c
pmecchia <mecchia.pierre@gmail.com>
2020-07-13 22:12:15 +0200

    redux persist

--- App.js
+++ App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import {Provider} from 'react-redux'
 import Navigation from './Navigation/Navigation'
 import Store from './Store/configureStore'
+import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/es/integration/react' //se charge de réhydrater ses components enfants
 
-export default class App extends React.Component{
-  render(){
-    return(
-      <Provider store = {Store}>
-        <Navigation/>
+export default class App extends React.Component {
+  render() {
+    let persistor = persistStore(Store) //pour persister et récupérer le store persisté
+    return (
+      <Provider store={Store}>
+        <PersistGate persistor={persistor}>
+          <Navigation />
+        </PersistGate>
       </Provider>
     )
   }
